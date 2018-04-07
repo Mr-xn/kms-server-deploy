8c68498a8de0d6fdf43421f14c4247fc05a0ee46
Mrxn <admin@mrxn.net>
2018-04-07 10:37:31 +0800

    update pic
    update pic

---
+++ kms-server-deploy.sh
@@ -0,0 +1,274 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+echo "#============================================================================="
+echo "# System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+"
+echo "# Description: deploy the vlmcsd server(windows系列的VL版本KMS激活服务端一键搭建)"
+echo "# Version: 1.0.0"
+echo "# Author:Mrxn"
+echo "# Date:04/05/2018"
+echo "# Blog:https://mrxn.net"
+echo "# kms_server:https://kms.mrxn.net"
+echo "#============================================================================="
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
+# 检查系统是否符合&是否已经安装vlmcsd服务
+check_root() {
+	[[ $EUID != 0 ]] && echo -e "${Error} 当前账号非ROOT(或没有ROOT权限)，无法继续操作，请使用${Green_background_prefix} sudo su ${Font_color_suffix}来获取临时ROOT权限（执行后会提示输入当前账号的密码）。" && exit 1
+}
+check_pid() {
+	PID=$(ps -ef | grep -v grep | grep vlmcsd | awk '{print $2}')
+	if [ ! -z $PID ]; then
+		STAT=0
+	else
+		STAT=1
+	fi
+}
+check_vlmcsd_start() {
+	check_pid
+	[[ ! -z ${PID} ]] && echo -e "${Error} vlmcsd正在运行,退出安装程序！" && exit 1
+}
+check_vlmcsd_stop() {
+	check_pid
+	[[ -z ${PID} ]] && echo -e "${Tip} 未发现vlmcsd服务,准备安装！"
+}
+check_vlmcsd_status() {
+	check_DAMON_status
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} vlmcsd正在运行,放心玩去吧！"
+	elif [ $STAT = 1 ]; then
+		echo -e "${Tip} 未发现vlmcsd服务,请尝试重启vlmcsd服务端或者是安装vlmcsd服务端"
+	fi
+}
+check_sys() {
+	if [[ -f /etc/redhat-release ]]; then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian"; then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu"; then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+		release="centos"
+	fi
+	bit=$(uname -m)
+}
+check_sys
+[[ ${release} != "debian" ]] && [[ ${release} != "ubuntu" ]] && [[ ${release} != "centos" ]] && echo -e "${Error} 本脚本不支持当前系统 ${release} !" && exit 1
+# 开始安装vlmcsd依赖
+install_depend() {
+	if [[ ${release} == "centos" ]]; then
+		yum install gcc git make -y
+	elif [[ ${release} == "debian" ]] || [[ ${release} == "ubuntu" ]]; then
+		apt-get install gcc git make -y
+	fi
+	echo -e "${Info} 依赖安装完毕...安装vlmcsd主程序..."
+
+}
+check_install_status() {
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} vlmcsd安装成功!"
+	elif [ $STAT = 1 ]; then
+		echo -e "${Error} vlmcsd安装失败!"
+	fi
+}
+# 开始从Wind4的仓库克隆到本地安装
+backup_old_vlmcsd() {
+	if [[ -f $DAEMON ]]; then
+		echo -e "${Tip} 发现旧的vlmcsd文件,将自动备份到/root/vlmcsd目录!"
+		mkdir /root/vlmcsd
+		mv $DAEMON /root/vlmcsd/old_vlmcsd
+	elif [[ ! -f $DAEMON ]]; then
+		echo -e "${Tip} 文件不存在,即将安装!"
+	fi
+}
+start_install() {
+	echo -e "${Info} 正在检查当前账号是否有执行权限部署操作..."
+	echo -e "${Info} 正在检查是否已经安装vlmcsd..."
+	echo -e "${Info} 恭喜,系统检查通过,检查是否安装vlmcsd服务端中..."
+	check_vlmcsd_stop
+	check_vlmcsd_start
+	backup_old_vlmcsd
+	echo -e "${Tip}${Red_font_prefix} 检查完毕,开始安装vlmcsd服务端...${Font_color_suffix}"
+	echo -e "${Info} 依赖安装/检查中..."
+	install_depend
+	mkdir /usr/local/kms
+	cd /usr/local/kms
+	git clone https://github.com/Wind4/vlmcsd.git
+	cd vlmcsd
+	make
+	cd bin
+	mv vlmcsd /usr/local/kms/kms
+	cd /usr/local/kms/
+	rm -rf ./vlmcsd/
+	mv kms vlmcsd
+	Add_iptables
+	Save_iptables
+	Set_iptables
+	vlmcsd_start
+	check_install_status
+}
+check_install() {
+	echo && stty erase '^H' && read -p "确认安装?[y/n]:" choice
+	case $choice in
+	"y")
+		start_install
+		;;
+	"n")
+		exit 0
+		;;
+	*)
+		echo "Please enter y or n!"
+		;;
+	esac
+}
+# 设置 防火墙规则
+Add_iptables() {
+	iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1688 -j ACCEPT
+}
+Del_iptables() {
+	iptables -D INPUT -m state --state NEW -m tcp -p tcp --dport 1688 -j ACCEPT
+}
+Save_iptables() {
+	if [[ ${release} == "centos" ]]; then
+		service iptables save
+	else
+		iptables-save >/etc/iptables.up.rules
+	fi
+}
+Set_iptables() {
+	if [[ ${release} == "centos" ]]; then
+		service iptables save
+		chkconfig --level 2345 iptables on
+	elif [[ ${release} == "debian" ]]; then
+		iptables-save >/etc/iptables.up.rules
+	elif [[ ${release} == "ubuntu" ]]; then
+		iptables-save >/etc/iptables.up.rules
+	fi
+}
+
+DAEMON=/usr/local/kms/vlmcsd
+STAT=2
+check_DAMON_status() {
+	[[ ! -f $DAEMON ]] && echo -e "${Error} 文件不存在,请安装!" && exit 1
+}
+vlmcsd_restart() {
+	check_DAMON_status
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} 重启vlmcsd服务端中..."
+		kill $PID
+	elif [ $STAT = 1 ]; then
+		echo -e "${Info} vlmcsd服务端未启动,启动中..."
+	fi
+	$DAEMON -L 0.0.0.0:1688 -l vlmcsd.log
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} Succeeded."
+	elif [ $STAT = 1 ]; then
+		echo -e "${Info} Failed."
+	fi
+}
+vlmcsd_stop() {
+	check_DAMON_status
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} 停止vlmcsd服务端..."
+		kill $PID
+		check_pid
+		if [ $STAT = 0 ]; then
+			echo -e "${Info} Failed."
+		elif [ $STAT = 1 ]; then
+			echo -e "${Info} Succeeded."
+		fi
+	elif [ $STAT = 1 ]; then
+		echo -e "${Info} vlmcsd没有运行"
+	fi
+}
+vlmcsd_start() {
+	check_DAMON_status
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} vlmcsd服务端已经运行."
+		exit 0
+	elif [ $STAT = 1 ]; then
+		echo -e "${Info} vlmcsd服务端未启动,启动中..."
+		$DAEMON -L 0.0.0.0:1688 -l vlmcsd.log
+	fi
+	check_pid
+	if [ $STAT = 0 ]; then
+		echo -e "${Info} Succeeded."
+	elif [ $STAT = 1 ]; then
+		echo -e "${Info} Failed."
+	fi
+}
+uninstall_vlmcsd() {
+	echo -e "${Tip} 正在卸载vlmcsd服务端..."
+	vlmcsd_stop
+	rm -rf $DAEMON
+	echo -e "${Tip} 卸载vlmcsd服务端完毕..."
+	Del_iptables
+	Save_iptables
+	echo -e "${Tip} 删除防火墙规则完毕..."
+}
+check_uninstall() {
+	check_DAMON_status
+	echo && stty erase '^H' && read -p "确认卸载?[y/n]:" choice
+	case $choice in
+	"y")
+		uninstall_vlmcsd
+		;;
+	"n")
+		exit 0
+		;;
+	*)
+		echo -e "${Tip} 请输入[y]或者[n]!"
+		;;
+	esac
+}
+echo -e "  vlmcsd服务端 一键管理脚本 ${Red_font_prefix}Powered By Mrxn.Net${Font_color_suffix}
+
+  ${Green_font_prefix}1.${Font_color_suffix} 安装vlmcsd服务端
+  ${Green_font_prefix}2.${Font_color_suffix} 查看vlmcsd的状态
+  ${Green_font_prefix}3.${Font_color_suffix} 启动vlmcsd服务端
+  ${Green_font_prefix}4.${Font_color_suffix} 停止vlmcsd服务端
+  ${Green_font_prefix}5.${Font_color_suffix} 重启vlmcsd服务端
+  ${Green_font_prefix}6.${Font_color_suffix} 卸载vlmcsd服务端
+  "
+
+echo && stty erase '^H' && read -p "请输入数字 [1-6]：" num
+case "$num" in
+1)
+	start_install
+	;;
+2)
+	check_vlmcsd_status
+	;;
+3)
+	vlmcsd_start
+	;;
+4)
+	vlmcsd_stop
+	;;
+5)
+	vlmcsd_restart
+	;;
+6)
+	check_uninstall
+	;;
+*)
+	echo -e "${Error} 请输入正确的数字 [1-6]"
+	;;
+esac
---
+++ mrxn_net_kms.cmd
@@ -0,0 +1,185 @@
+chcp 936
+@echo off&PUSHD  &TITLE Mrxn_Blog_KMS激活工具
+mode con cols=75 lines=25
+setlocal EnableDelayedExpansion&color 70 & cd /d "%~dp0"
+%1 %2
+ver|find "5.">nul&&goto :start
+mshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :start","","runas",1)(window.close)&goto :eof
+
+:start
+set KMS_Sev=kms.mrxn.net
+cls
+echo VL版本系列windows和office激活脚本;
+echo 首先感谢vlmcsd的作者,其GitHub项目地址:https://github.com/Wind4/vlmcsd
+echo PC脚本作者:Mrxn;                                                   
+echo ======================================================
+echo m    m                             mm   m          m
+echo ##  ##  m mm  m   m  m mm          #"m  #  mmm   mm#mm
+echo # ## #  #"  "  #m#   #"  #         # #m # #"  #    #
+echo # "" #  #      m#m   #   #         #  # # #""""    #
+echo #    #  #     m" "m  #   #    #    #   ## "#mm"    "mm
+echo =======================================================                                                       
+
+echo 我的个人博客:https://mrxn.net/;
+echo kms服务器地址:kms.mrxn.net,方便喜欢手动激活的朋友;
+echo 有问题请反馈在我的博客留言，或者发送至我的邮箱:admin@mrxn.net;
+echo 正在检查与激活服务器的连接情况......请耐心等待;
+echo.
+ping kms.mrxn.net | find "超时"  > NUL &&  goto fail
+ping kms.mrxn.net | find "目标主机"  > NUL &&  goto fail
+echo 成功连接上服务器……
+ver | find "6.0." > NUL &&  goto winvista
+ver | find "6.1." > NUL &&  goto win7
+ver | find "6.2." > NUL &&  goto win8
+ver | find "6.3." > NUL &&  goto win81
+ver | find "10.0." > NUL &&  goto win10
+echo 未找到合适的NT6系统，可能是WinXP或Win2003。
+goto office
+
+:winvista
+echo 当前为Windows Vista/2008。
+set Business=YFKBB-PQJJV-G996G-VWGXY-2V3X8
+set BusinessN=HMBQG-8H2RH-C77VX-27R82-VMQBT
+set Enterprise=VKK3X-68KWM-X2YGT-QR4M6-4BWMV
+set EnterpriseN=VTC42-BM838-43QHV-84HX6-XJXKV
+set ServerWeb=WYR28-R7TFJ-3X2YQ-YCY4H-M249D
+set ServerStandard=TM24T-X9RMF-VWXK6-X8JC9-BFGM2
+set ServerStandardV=W7VD6-7JFBR-RX26B-YKQ3Y-6FFFJ
+set ServerEnterprise=YQGMW-MPWTJ-34KDK-48M3W-X4Q6V
+set ServerEnterpriseV=39BXF-X8Q23-P2WWT-38T2F-G3FPG
+set ServerWeb=RCTX3-KWVHP-BR6TB-RB6DM-6X7HP
+set ServerDatacenter=7M67G-PC374-GR742-YH8V4-TCBY3
+set ServerDatacenterV=22XQ2-VRXRG-P8D42-K34TD-G3QQC
+set ServerEnterpriseIA64=4DWFP-JF3DJ-B7DTH-78FJB-PDRHK
+goto windowsstart
+
+:win7
+echo 当前为Windows 7/2008 R2。
+set Professional=FJ82H-XT6CR-J8D7P-XQJJ2-GPDD4
+set ProfessionalN=MRPKT-YTG23-K7D7T-X2JMM-QY7MG
+set ProfessionalE=W82YF-2Q76Y-63HXB-FGJG9-GF7QX
+set Enterprise=33PXH-7Y6KF-2VJC9-XBBR8-HVTHH
+set EnterpriseN=YDRBP-3D83W-TY26F-D46B2-XCKRJ
+set EnterpriseE=C29WB-22CC8-VJ326-GHFJW-H9DH4
+set ServerWeb=6TPJF-RBVHG-WBW2R-86QPH-6RTM4
+set ServerHPC=TT8MH-CG224-D3D7Q-498W2-9QCTX
+set ServerStandard=YC6KT-GKW9T-YTKYR-T4X34-R7VHC
+set ServerEnterprise=489J6-VHDMP-X63PK-3K798-CPX3Y
+set ServerDatacenter=74YFP-3QFB3-KQT8W-PMXWJ-7M648
+set ServerEnterpriseIA64=GT63C-RJFQ3-4GMB6-BRFB9-CB83V
+goto windowsstart
+:win8
+echo 当前为Windows 8/2012。
+set Professional=NG4HW-VH26C-733KW-K6F98-J8CK4
+set ProfessionalN=XCVCF-2NXM9-723PB-MHCB7-2RYQQ
+set Core=BN3D2-R7TKB-3YPBD-8DRP2-27GG4
+set Enterprise=32JNW-9KQ84-P47T8-D8GGY-CWCK7
+set EnterpriseN=JMNMF-RHW7P-DMY6X-RF3DR-X2BQT
+set CoreN=8N2M2-HWPGY-7PGT9-HGDD8-GVGGY
+set CoreSingleLanguage=2WN2H-YGCQR-KFX6K-CD6TF-84YXQ
+set CoreCountrySpecific=4K36P-JN4VD-GDC6V-KDT89-DYFKP
+set ServerMultiPointPremium=XNH6W-2V9GX-RGJ4K-Y8X6F-QGJ2G
+set ServerMultiPointStandard=HM7DN-YVMH3-46JC3-XYTG7-CYQJJ
+set ServerStandard=XC9B7-NBPP2-83J2H-RHMBY-92BT4
+set ServerDatacenter=48HP8-DN98B-MYWDG-T2DCC-8W83P
+
+goto windowsstart
+:win81
+echo 当前为Windows 8.1。
+set Professional=GCRJD-8NW9H-F2CDX-CCM8D-9D6T9
+set ProfessionalN=HMCNV-VVBFX-7HMBH-CTY9B-B4FXY
+set Enterprise=MHF9N-XY6XB-WVXMC-BTDCT-MKKG7
+set EnterpriseN=TT4HM-HN7YT-62K67-RGRQJ-JFFXW
+set ServerSolution=KNC87-3J2TX-XB4WP-VCPJV-M4FWM
+set ServerStandard=D2N9P-3P6X9-2R39C-7RTCD-MDVJX
+set ServerDatacenter=W3GGN-FT8W3-Y4M27-J84CP-Q3VJ9
+goto windowsstart
+:win10
+echo 当前为Windows 10。
+set Core=TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+set CoreCountrySpecific=PVMJN-6DFY6-9CCP6-7BKTT-D3WVR
+set CoreN=3KHY7-WNT83-DGQKR-F7HPR-844BM
+set CoreSingleLanguage=7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+set Professional=W269N-WFGWX-YVC9B-4J6C9-T83GX
+set ProfessionalN=MH37W-N47XK-V7XM9-C7227-GCQG9
+set Enterprise=NPPR9-FWDCX-D2C8J-H872K-2YT43
+set EnterpriseN=DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+set Education=NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+set EducationN=2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+set EnterpriseS=WNMTR-4C88C-JK8YV-HQ7T2-76DF9
+set EnterpriseSN=2F77B-TNFGY-69QQF-B8YKP-D69TJ
+goto windowsstart
+:windowsstart
+for /f "tokens=3 delims= " %%i in ('reg QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "EditionID"') do set EditionID=%%i
+if defined %EditionID% (
+	cscript //Nologo %windir%\system32\slmgr.vbs /ipk !%EditionID%!
+	cscript //Nologo %windir%\system32\slmgr.vbs /skms kms.mrxn.net
+	cscript //Nologo %windir%\system32\slmgr.vbs /ato
+) else (
+	echo 找不到系列号，可能是旗舰版之类的系统……
+)
+goto office
+:office
+echo 检查安装的office……
+call :GetOfficePath 14 Office2010
+call :ActOffice 14 Office2010
+call :GetOfficePath 15 Office2013
+call :ActOffice 15 Office2013
+if exist "%ProgramFiles%\Microsoft Office\Office16\ospp.vbs" set _Office16Path=%ProgramFiles%\Microsoft Office\Office16
+if exist "%ProgramFiles(x86)%\Microsoft Office\Office16\ospp.vbs" set _Office16Path=%ProgramFiles(x86)%\Microsoft Office\Office16
+if DEFINED _Office16Path (echo.&echo 已发现 Office2016
+    call :ActOffice 16 Office2016
+  ) else (echo.&echo 未发现 Office2016)
+
+
+echo.&pause
+exit
+
+:ActOffice
+if DEFINED _Office%1Path (
+    cd /d "!_Office%1Path!"
+    if %1 EQU 16 call :Licens16
+    echo.&echo 尝试激活 %2 ...&echo.
+    cscript //nologo ospp.vbs /sethst:kms.mrxn.net >nul
+    cscript //nologo ospp.vbs /act | find /i "successful" && (
+        echo.&echo ***** %2 激活成功 ***** & echo.) || (echo.&echo ***** %2 激活失败 ***** & echo.)
+)    
+cd /d "%~dp0"
+goto :EOF
+
+:GetOfficePath
+echo.&echo 正在检测 %2 系列产品的安装路径...
+set _Office%1Path=
+set _Reg32=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\%1.0\Common\InstallRoot
+set _Reg64=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Office\%1.0\Common\InstallRoot
+reg query "%_Reg32%" /v "Path" > nul 2>&1 && FOR /F "tokens=2*" %%a IN ('reg query "%_Reg32%" /v "Path"') do SET "_OfficePath1=%%b"
+reg query "%_Reg64%" /v "Path" > nul 2>&1 && FOR /F "tokens=2*" %%a IN ('reg query "%_Reg64%" /v "Path"') do SET "_OfficePath2=%%b"
+if DEFINED _OfficePath1 (if exist "%_OfficePath1%ospp.vbs" set _Office%1Path=!_OfficePath1!)
+if DEFINED _OfficePath2 (if exist "%_OfficePath2%ospp.vbs" set _Office%1Path=!_OfficePath2!)
+set _OfficePath1=
+set _OfficePath2=
+if DEFINED _Office%1Path (echo.&echo 已发现 %2) else (echo.&echo 未发现 %2)
+goto :EOF
+
+:Licens16
+for /f %%x in ('dir /b ..\root\Licenses16\project???vl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\standardvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\visio???vl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\project???vl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\proplusvl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\standardvl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+for /f %%x in ('dir /b ..\root\Licenses16\visio???vl_mak*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%%x" >nul
+cscript ospp.vbs /inpkey:NYH39-6GMXT-T39D4-WVXY2-D69YY >nul
+cscript ospp.vbs /inpkey:7WHWN-4T7MP-G96JF-G33KR-W8GF4 >nul
+cscript ospp.vbs /inpkey:RBWW7-NTJD4-FFK2C-TDJ7V-4C2QP >nul
+cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99 >nul
+cscript ospp.vbs /inpkey:YG9NW-3K39V-2T3HJ-93F3Q-G83KT >nul
+cscript ospp.vbs /inpkey:PD3PC-RHNGV-FXJ29-8JK7D-RJRJK >nul
+goto :EOF
+
+exit
+:fail
+cls
+echo 无法连接到服务器……
+pause
